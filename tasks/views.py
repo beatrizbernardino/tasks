12d56fd1e81c9827f3a126ea0ec628e6626f3b87
Beatriz Rianho Bernadinho <pbeatriz18@hotmail.com.br>
2021-12-01 18:27:04 -0300

    json

--- tasks/views.py
+++ tasks/views.py
@@ -38,5 +38,5 @@ def details_task(request):
         serializer = TaskSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer, status=status.HTTP_201_CREATED)
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
